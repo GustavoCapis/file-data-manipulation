bbe4e949119981d74b647534ea23ce7a8cf47a4c
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-03-19 11:19:29 -0300

    feat: implementar função main

--- exercicios/ex02.py
+++ exercicios/ex02.py
@@ -37,3 +37,18 @@ def mover_arquivos(diretorio_origem):
                     print(f"Erro inesperado: {e} ao mover {arquivo}.")
             else:
                 print(f"Extensão {extensao} de {arquivo} não é suportada.")
+                
+def main():
+    diretorio_trabalho = "diretorio_trabalho"
+    diretorios = [os.path.join(diretorio_trabalho, 'pdf'),
+                  os.path.join(diretorio_trabalho, 'txt'),
+                  os.path.join(diretorio_trabalho, 'jpg'),
+                  ]
+    #Criar diretórios se não existirem
+    criar_diretorios(diretorios)
+    
+    #Mover arquivos para os diretórios correspondetes
+    mover_arquivos(diretorio_trabalho)
+    
+if __name__ == "__main__":
+    main()
